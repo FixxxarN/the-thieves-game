2a7a6f90a8cc2caf0fc1c0cfcccb56e952fe9867
FixxxarN <tornstromandre@gmail.com>
2020-10-19 12:17:09 +0200

    Added a player script and a walking module for the player

---
+++ The Thieves/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _health;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _speed;
+
+    public float Health
+    {
+        private set { _health = value; }
+        get { return _health; }
+    }
+
+    public float Damage
+    {
+        private set { _damage = value; }
+        get { return _damage; }
+    }
+
+    public float Speed
+    {
+        private set { _speed = value; }
+        get { return _speed; }
+    }
+
+    public Player()
+    {
+        _health = 100f;
+        _damage = 10f;
+        _speed = 1f;
+    }
+}
---
+++ The Thieves/Assets/Scripts/PlayerWalkingModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkingModule : MonoBehaviour
+{
+    private Player _player;
+    private Rigidbody2D _rigidBody;
+
+    void Start()
+    {
+        _player = GetComponent<Player>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            _rigidBody.velocity = new Vector2(-_player.Speed, _rigidBody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rigidBody.velocity = new Vector2(_player.Speed, _rigidBody.velocity.y);
+        }
+        else
+        {
+            _rigidBody.velocity = new Vector2(0, _rigidBody.velocity.y);
+        }
+    }
+}
