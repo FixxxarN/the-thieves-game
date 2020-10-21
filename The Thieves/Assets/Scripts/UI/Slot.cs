bad896dca6261be3c5b146b8b7a10c2da24892a0
FixxxarN <tornstromandre@gmail.com>
2020-10-21 22:51:27 +0200

    Fixed player health ui, active player ui and a basic dialog. I also added a random items container with a pistol, a banana peel and a grenade.

---
+++ The Thieves/Assets/Scripts/UI/Slot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class Slot : MonoBehaviour
+    {
+        [SerializeField] private Item _item;
+        private Image _itemImage;
+
+        void Start()
+        {
+            _itemImage = transform.Find("Item").GetComponent<Image>();
+            _itemImage.sprite = _item.Texture;
+            _itemImage.SetNativeSize();
+        }
+    }
+}
